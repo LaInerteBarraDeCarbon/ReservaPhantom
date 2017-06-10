fa9dea764c1a632ae68ca8d56912b4ae470c230a
Damian Alladio <damian.e.alladio@gmail.com>
2017-06-09 23:58:32 -0300

    Carga Completa

--- src/reserva/Reserva.java
+++ src/reserva/Reserva.java
@@ -1,11 +1,69 @@
 package reserva;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 import java.util.Stack;
+
+import org.junit.experimental.theories.Theories;
 
 public class Reserva {
 	// Usar DFS
-	private int cantidadDescansos;
+	private int cantidadMiradores;
 	private int cantidadTramos;
+	private int cantidadCaminos = 0;
 	private Stack<Integer> pila;
-	
+	private int[] inicio;
+	private int[] destino;
+
+	public Reserva(String path) {
+		try {
+			leerArchivo(path);
+		} catch (Exception e) {
+			System.out.println("Error abrir archivo.");
+			e.printStackTrace();
+		}
+	}
+
+	public void leerArchivo(String path) {
+		Scanner sc = null;
+		try {
+			sc = new Scanner(new File(path));
+			this.cantidadMiradores = sc.nextInt();
+			this.cantidadTramos = sc.nextInt();
+			this.inicio = new int[this.cantidadTramos];
+			this.destino = new int[this.cantidadTramos];
+			this.pila = new Stack<Integer>();
+			for (int i = 0; i < this.cantidadTramos; i++) {
+				this.inicio[i] = sc.nextInt();
+				this.destino[i] = sc.nextInt();
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			sc.close();
+		}
+		sc.close();
+	}
+
+	public void escribirVector() {
+		for (int i = 0; i < this.cantidadTramos; i++)
+			System.out.println(this.inicio[i] + " " + this.destino[i] + "\n");
+	}
+
+	public void grabarArchivo(String path) {
+		PrintWriter salida;
+		try {
+			salida = new PrintWriter(new FileWriter(path));
+			salida.println(this.cantidadCaminos);
+
+			salida.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- test/reserva/ReservaTest.java
+++ test/reserva/ReservaTest.java
@@ -8,10 +8,11 @@ public class ReservaTest {
 
 	private static String archivoIn = "Preparacion de Prueba/Lote de Prueba/Entrada/";
 	private static String archivoOut = "Ejecucion de Prueba/Salida Obtenida/";
-	
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEnunciado() {
+		Reserva reserva = new Reserva(archivoIn + "00_Enunciado.in");
+		reserva.escribirVector();
 	}
 
 }
