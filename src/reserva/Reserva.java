3a012edf5a8f32d482f5ba9881e6c9322905e147
Damian Alladio <damian.e.alladio@gmail.com>
2017-06-09 21:38:12 -0300

    Carpetas Básicas

---
+++ src/reserva/Reserva.java
@@ -0,0 +1,11 @@
+package reserva;
+
+import java.util.Stack;
+
+public class Reserva {
+	// Usar DFS
+	private int cantidadDescansos;
+	private int cantidadTramos;
+	private Stack<Integer> pila;
+	
+}
---
+++ test/reserva/ReservaTest.java
@@ -0,0 +1,17 @@
+package reserva;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReservaTest {
+
+	private static String archivoIn = "Preparacion de Prueba/Lote de Prueba/Entrada/";
+	private static String archivoOut = "Ejecucion de Prueba/Salida Obtenida/";
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
